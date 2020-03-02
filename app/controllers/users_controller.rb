2d1193385b4a7451c88cd95269496b92253ae17e
github username <github email address>
2020-03-01 19:00:04 -0700

    Add authorization methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,26 @@
 class UsersController < ApplicationController
     def create 
-        @user = User.create( 
-            username: params[:username],
-            password: params[:password]
-        )
-        render json: @user 
+        @user = User.new(user_params) 
+        if @user.save 
+            render json: @user, status: :created
+        else 
+            render json: {error: "Cannot recognize credentials"}, status: :bad_request
+        end 
     end 
 
     def index 
         begin 
         authenticate 
-        @user = User.all 
+        @users = User.all 
 
-        render json: @user
+        render json: @users
         rescue 
+        end
+    end 
+
+    private 
+
+    def user_params 
+        params.permit(:username, :password)
     end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :days, only: [:create]
-  get "/bulk", to: "workouts#create_bulk_split"
-  get "/cut", to: "workouts#create_cut_split"
+  get "bulk", to: "workouts#create_bulk_split"
+  get "cut", to: "workouts#create_cut_split"
   # post "/days", to: "days#create_5_days"
   resources :workouts, only: [:index, :show, :create]
   resources :users, only: [:index, :create]
-  post "/login", to: "authentication#login"
+  post "login", to: "authentication#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
