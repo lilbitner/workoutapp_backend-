af85311342a730052165c8263bd9ce32968570d2
github username <github email address>
2020-03-08 17:45:06 -0600

    Minor syntax changes

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,17 +2,17 @@ class AuthenticationController < ApplicationController
     def login 
         @user = User.find_by(username: params[:username])
     
-    if !@user 
+        if !@user 
         render json: {message: "Wrong user credentials"}, status: :unauthorized
-    else 
-        if !@user.authenticate(params[:password])
-            render json: {message: "Wrong password"}, status: :unauthorized
         else 
+            if !@user.authenticate(params[:password])
+            render json: {message: "Wrong password"}, status: :unauthorized
+            else 
             payload = {id: @user.id} 
             secret = Rails.application.secrets.secret_key_base
             token = JWT.encode(payload, secret)
             render json: {token: token, user_id: @user.id, username: @user.username} 
+            end 
         end 
-    end 
-end
+    end
 end
